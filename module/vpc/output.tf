ab413843f5d07ffe4b8ebe0168447486cd076479
Mcloudhight <michael.ofeimu@cloudhight.com>
2023-09-20 17:51:15 -0400

    created vpc

---
+++ module/vpc/output.tf
@@ -0,0 +1,18 @@
+# # output "ansible" {
+#   value = module.ansible.ansible-ip
+# }
+# output "sonarqube" {
+#   value = module.sonarqube.sonarqube_ip
+# }
+# output "bastion" {
+#   value = module.bastion.bastion
+# }
+# output "nexus" {
+#   value = module.nexus_server.nexus_public-ip
+# }
+# output "jenkins" {
+#   value = module.jenkins.jenkins_server_ip
+# }
+# output "docker" {
+#   value = module.docker.docker_server_ip
+# }
